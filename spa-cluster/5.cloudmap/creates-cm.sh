0d78126d9417fb41222e8bc3c7dcbb4fba30975c
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2024-12-15 10:10:27 +0530

    made the changes

--- spa-cluster/5.cloudmap/creates-cm.sh
+++ spa-cluster/5.cloudmap/creates-cm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 aws servicediscovery create-service \
     --name spa-backend \
-    --namespace-id ns-5zsaptwcvbg3ve2z \
-    --dns-config "NamespaceId=ns-5zsaptwcvbg3ve2z,RoutingPolicy=WEIGHTED,DnsRecords=[{Type=A,TTL=60}]"
+    --namespace-id ns-asl6syvjpdvlthvc \
+    --dns-config "NamespaceId=ns-asl6syvjpdvlthvc,RoutingPolicy=WEIGHTED,DnsRecords=[{Type=A,TTL=60}]"
 
