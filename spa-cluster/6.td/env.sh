11cb3b08ad60a12bb48677fede8c55e9c99dfd9f
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-01-19 16:43:50 +0530

    td was updated

---
+++ spa-cluster/6.td/env.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+aws ecs register-task-definition \
+    --family spa-backend \
+    --network-mode awsvpc \
+    --requires-compatibilities FARGATE \
+    --cpu "256" \
+    --memory "512" \
+    --execution-role-arn arn:aws:iam::522814728660:role/ecsSpaTaskExecution \
+    --container-definitions '[
+        {
+            "name": "backend",
+            "image": "siva9666/spa-backend:v1",
+            "environment": [
+                {
+                    "name": "DB_HOST",
+                    "value": "spa-rds.bapatlas.site"
+                },
+                {
+                    "name": "DB_USER",
+                    "value": "crud"
+                },
+                {
+                    "name": "DB_PASSWORD",
+                    "value": "CrudApp1"
+                },
+                {
+                    "name": "DB_NAME",
+                    "value": "crud_app"
+                },
+                {
+                    "name": "PORT",
+                    "value": "3306"
+                },
+                {
+                    "name": "ALLOWED_ORIGIN",
+                    "value": "https://spa-app.bapatlas.site"
+                }
+            ],
+            "portMappings": [
+                {
+                    "containerPort": 8080,
+                    "protocol": "tcp"
+                }
+            ],
+            "logConfiguration": {
+                "logDriver": "awslogs",
+                "options": {
+                    "awslogs-group": "/ecs/spa-backend",
+                    "awslogs-region": "us-east-1",
+                    "awslogs-stream-prefix": "ecs"
+                }
+            }
+        }
+    ]'
