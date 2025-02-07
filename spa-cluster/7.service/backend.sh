0b5338bb630088a8fd1aa7eeb67de741d0b9d64f
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-02-07 10:28:46 +0530

    update the changes

--- spa-cluster/7.service/backend.sh
+++ spa-cluster/7.service/backend.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+CLUSTER_NAME="spa"
+SERVICE_NAME="backend"
+
 aws ecs create-service \
-    --cluster spa \
-    --service-name backend \
+    --cluster "$CLUSTER_NAME" \
+    --service-name "$SERVICE_NAME" \
     --task-definition spa-backend \
     --desired-count 1 \
     --launch-type FARGATE \
     --network-configuration "awsvpcConfiguration={subnets=[subnet-01899a28d9cd091c2,subnet-000f164cabd01ad15],securityGroups=[sg-0c2026150f42233ac],assignPublicIp=ENABLED}" \
     --service-registries "registryArn=arn:aws:servicediscovery:us-east-1:522814728660:service/srv-4hovdpr7nf6yklcn"
+
+
+aws ecs update-service \
+    --cluster "$CLUSTER_NAME" \
+    --service "$SERVICE_NAME" \
+    --load-balancers targetGroupArn=arn:aws:elasticloadbalancing:us-east-1:522814728660:targetgroup/backend/8ff22fb8fb29314d,containerName=backend,containerPort=8080
+echo "ECS Service updated with Target Group."
