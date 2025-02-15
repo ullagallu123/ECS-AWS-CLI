f42b580f127170ccd7a870861e545c8f82fa8462
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-02-15 17:07:02 +0530

    update the alb

--- 7.service/debug.sh
+++ 7.service/debug.sh
@@ -6,5 +6,5 @@ aws ecs create-service \
     --task-definition debug-utility \
     --desired-count 1 \
     --launch-type FARGATE \
-    --network-configuration "awsvpcConfiguration={subnets=[subnet-090cf41d5e0cdf437],securityGroups=[sg-0ca8167841704f04d],assignPublicIp=ENABLED}" \
-    --service-registries "registryArn=arn:aws:servicediscovery:ap-south-1:4273-6630-1535:service/srv-4gu3pirxufusoymk"
+    --network-configuration "awsvpcConfiguration={subnets=[subnet-01899a28d9cd091c2],securityGroups=[sg-0c2026150f42233ac],assignPublicIp=ENABLED}" \
+    --service-registries "registryArn=arn:aws:servicediscovery:us-east-1:522814728660:service/srv-qpfsrtmgp2wt6m6f"
