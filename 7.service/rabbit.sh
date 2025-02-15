4b42ef7e49e65023c7968565ddf6641ed5da28e3
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-02-15 16:48:14 +0530

    update the all srv

--- 7.service/rabbit.sh
+++ 7.service/rabbit.sh
@@ -5,5 +5,5 @@ aws ecs create-service \
     --task-definition rabbit \
     --desired-count 1 \
     --launch-type FARGATE \
-    --network-configuration "awsvpcConfiguration={subnets=[subnet-090cf41d5e0cdf437],securityGroups=[sg-0ca8167841704f04d],assignPublicIp=ENABLED}" \
-    --service-registries "registryArn=arn:aws:servicediscovery:ap-south-1:4273-6630-1535:service/srv-23aa6nqe4xy5acpz"
+    --network-configuration "awsvpcConfiguration={subnets=[subnet-01899a28d9cd091c2],securityGroups=[sg-0c2026150f42233ac],assignPublicIp=ENABLED}" \
+    --service-registries "registryArn=arn:aws:servicediscovery:us-east-1:522814728660:service/srv-sd3izpsprwycqezb"
